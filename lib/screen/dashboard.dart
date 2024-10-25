7dfd03198d8d46c33dcd0fac8fe8dc3e7c8f11d2
Jedy24 <reihimawari27@gmail.com>
2024-10-25 21:43:51 +0700

    Dashboard with firebase integrated

--- lib/screen/dashboard.dart
+++ lib/screen/dashboard.dart
@@ -3,6 +3,9 @@ import 'package:google_fonts/google_fonts.dart';
 import 'balance.dart';
 import 'add_expenses.dart';
 import 'payment.dart';
+import 'package:intl/intl.dart';
+import 'package:finance_tracker/services/expense_service.dart';
+import 'package:finance_tracker/components/currency_formatter.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
@@ -17,7 +20,6 @@ class DashboardScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // Header dengan teks Hello dan Home, serta tombol Add dan Pay
               Padding(
                 padding: const EdgeInsets.only(top: 40.0), // Jarak dari atas
                 child: Column(
@@ -40,7 +42,7 @@ class DashboardScreen extends StatelessWidget {
                           'Home',
                           style: GoogleFonts.inter(
                             textStyle: const TextStyle(
-                              color: Color(0xFF007AFF), // Warna biru #007AFF
+                              color: Color(0xFF007AFF),
                               fontSize: 34,
                               fontWeight: FontWeight.w700,
                             ),
@@ -50,8 +52,8 @@ class DashboardScreen extends StatelessWidget {
                           children: [
                             // Tombol Add
                             Container(
-                              width: 78, // Set lebar sesuai yang diinginkan
-                              height: 40, // Set tinggi sesuai yang diinginkan
+                              width: 78,
+                              height: 40, 
                               decoration: BoxDecoration(
                                 gradient: const LinearGradient(
                                   colors: [Color(0xFF12B0F8), Color(0xFF007AFF)],
@@ -81,8 +83,8 @@ class DashboardScreen extends StatelessWidget {
                             const SizedBox(width: 8),
                             // Tombol Pay
                             Container(
-                              width: 78, // Set lebar sesuai yang diinginkan
-                              height: 40, // Set tinggi sesuai yang diinginkan
+                              width: 78,
+                              height: 40, 
                               decoration: BoxDecoration(
                                 gradient: const LinearGradient(
                                   colors: [Color(0xFF12B0F8), Color(0xFF007AFF)],
@@ -118,59 +120,80 @@ class DashboardScreen extends StatelessWidget {
               ),
               const SizedBox(height: 24),
 
-              // Total pengeluaran bulanan dan harian (tengah layar)
+              // Total pengeluaran bulanan dan harian
               Center(
                 child: Card(
                   color: Colors.white,
                   elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
                   child: Padding(
                     padding: const EdgeInsets.all(16.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         Text(
-                          '[Monthly] Expenses',
+                          '${DateFormat.MMMM().format(DateTime.now())} Expenses',
                           style: GoogleFonts.inter(
                             textStyle: const TextStyle(
-                              color: Color(0xFF007AFF), // Warna biru #007AFF
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 4),
-                        Text(
-                          'Rp. 250.520',
-                          style: GoogleFonts.inter(
-                            textStyle: const TextStyle(
-                              fontSize: 32,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 16),
-                        Text(
-                          '[Daily] Expenses',
-                          style: GoogleFonts.inter(
-                            textStyle: const TextStyle(
-                              color: Color(0xFF007AFF), // Warna biru #007AFF
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 4),
-                        Text(
-                          'Rp. 25.520',
-                          style: GoogleFonts.inter(
-                            textStyle: const TextStyle(
+                              color: Color(0xFF007AFF), 
                               fontSize: 20,
                               fontWeight: FontWeight.w600,
                             ),
                           ),
+                        ),
+                        const SizedBox(height: 4),
+                        FutureBuilder<double>(
+                          future: getMonthlyExpenses(),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState == ConnectionState.waiting) {
+                              return const CircularProgressIndicator(); // Placeholder saat loading
+                            }
+                            if (snapshot.hasError) {
+                              return Text('Error: ${snapshot.error}');
+                            }
+                            return Text(
+                              CurrencyFormatter.formatCurrency(snapshot.data),
+                              style: GoogleFonts.inter(
+                                textStyle: const TextStyle(
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+
+                        const SizedBox(height: 16),
+                        Text(
+                          '${DateFormat('d MMM yyyy').format(DateTime.now())} Expenses',
+                          style: GoogleFonts.inter(
+                            textStyle: const TextStyle(
+                              color: Color(0xFF007AFF),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        FutureBuilder<double>(
+                          future: getDailyExpenses(),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState == ConnectionState.waiting) {
+                              return const CircularProgressIndicator(); 
+                            } else if (snapshot.hasError) {
+                              return const Text('Error loading data'); 
+                            } else {
+                              final dailyExpense = snapshot.data ?? 0;
+                              return Text(
+                                CurrencyFormatter.formatCurrency(dailyExpense), 
+                                style: GoogleFonts.inter(
+                                  textStyle: const TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              );
+                            }
+                          },
                         ),
                       ],
                     ),
@@ -182,9 +205,6 @@ class DashboardScreen extends StatelessWidget {
               Card(
                 color: Colors.white,
                 elevation: 0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: Column(
@@ -194,7 +214,7 @@ class DashboardScreen extends StatelessWidget {
                         'Expenses',
                         style: GoogleFonts.inter(
                           textStyle: const TextStyle(
-                            color: Color(0xFF007AFF), // Warna biru #007AFF
+                            color: Color(0xFF007AFF),
                             fontSize: 20,
                             fontWeight: FontWeight.w600,
                           ),
@@ -222,106 +242,98 @@ class DashboardScreen extends StatelessWidget {
               Card(
                 color: Colors.white,
                 elevation: 0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Row(
+                     Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Text(
-                            'History [Highest]',
+                            'Highest Expenses',
                             style: GoogleFonts.inter(
                               textStyle: const TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w600,
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
                           ),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => BalancePage()),
-                              );
-                            },
-                            child: Text(
-                              'See all',
-                              style: GoogleFonts.inter(
-                                textStyle: const TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w500,
+                          Padding(
+                            padding: const EdgeInsets.only(right: 0),
+                            child: TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => BalancePage()),
+                                );
+                              },
+                              style: TextButton.styleFrom(
+                                padding: EdgeInsets.zero,
+                                minimumSize: Size.zero,
+                                alignment: Alignment.centerRight,
+                              ),
+                              child: Text(
+                                'See all',
+                                style: GoogleFonts.inter(
+                                  textStyle: const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w500,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
                         ],
                       ),
-                      const SizedBox(height: 8),
-                      ListTile(
-                        leading: Container(
-                          width: 24,
-                          height: 24,
-                          color: Colors.blue,
-                        ),
-                        title: Text(
-                          'Nugget Frozen',
-                          style: GoogleFonts.inter(
-                            textStyle: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ),
-                        subtitle: Text(
-                          '10 Mar 2020',
-                          style: GoogleFonts.inter(),
-                        ),
-                        trailing: Text(
-                          '-\$799.00',
-                          style: GoogleFonts.inter(
-                            textStyle: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.blue
-                            ),
-                          ),
-                        ),
-                      ),
-                      ListTile(
-                        leading: Container(
-                          width: 24,
-                          height: 24,
-                          color: Colors.grey,
-                        ),
-                        title: Text(
-                          'Income',
-                          style: GoogleFonts.inter(
-                            textStyle: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ),
-                        subtitle: Text(
-                          '5 Mar 2020',
-                          style: GoogleFonts.inter(),
-                        ),
-                        trailing: Text(
-                          '+\$50.00',
-                          style: GoogleFonts.inter(
-                            textStyle: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.blue
-                            ),
-                          ),
-                        ),
+
+                      FutureBuilder<List<Map<String, dynamic>>>(
+                        future: getHighestExpenses(),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState == ConnectionState.waiting) {
+                            return const CircularProgressIndicator();
+                          }
+                          if (snapshot.hasError) {
+                            return Text('Error: ${snapshot.error}');
+                          }
+                          final expenses = snapshot.data?.take(3).toList() ?? []; // Menampilkan maksimal 3 item
+                          return ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            padding: EdgeInsets.zero,
+                            itemCount: expenses.length,
+                            itemBuilder: (context, index) {
+                              final expense = expenses[index];
+                              return ListTile(
+                                dense: true,
+                                contentPadding: const EdgeInsets.symmetric(horizontal: 0),
+                                title: Text(
+                                  expense['category'].toString().split(' ').map((word) => word[0].toUpperCase() + word.substring(1)).join(' '), // Huruf kapital di awal kata
+                                  style: const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.bold,
+                                    ),
+                                ),
+                                subtitle: Text(
+                                  DateFormat('d MMM yyyy').format(expense['date'].toDate()),
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                    color: Color(0xFF8E8E93),
+                                  ),
+                                  ),
+                                trailing: Text(
+                                  CurrencyFormatter.formatCurrency(expense['amount']),
+                                  style: const TextStyle(
+                                    color: Colors.blue,
+                                    fontSize: 17, 
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        },
                       ),
                     ],
                   ),
