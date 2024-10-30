0498e922ad7a45ee2284eb080659cc8199500e73
Jedy24 <reihimawari27@gmail.com>
2024-10-30 21:07:31 +0700

    Update create expenses logic

--- lib/components/custom_categories.dart
+++ lib/components/custom_categories.dart
@@ -1,29 +1,36 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-
 class CustomCategories {
   List<String> _categories = [];
 
-  // Method untuk mengambil daftar kategori dari Firestore
   Future<List<String>> getCategories() async {
     final QuerySnapshot snapshot = await FirebaseFirestore.instance.collection('categories').get();
     return snapshot.docs.map((doc) => doc['name'] as String).toList();
   }
 
-  // Method untuk menambah kategori baru ke dalam Firestore
   Future<void> addNewCategory(String category) async {
     await FirebaseFirestore.instance.collection('categories').add({
       'name': category,
     });
-    await loadCategories(); // Memperbarui daftar kategori
+    await loadCategories();
   }
 
-  // Method untuk menambah pengeluaran ke dalam Firestore
   Future<void> addExpense(double amount, String category, DateTime date) async {
-    await FirebaseFirestore.instance.collection('expenses').add({
-      'amount': amount,
-      'category': category,
-      'date': Timestamp.fromDate(date),
-    });
+    if (category == "Installment") {
+      // Add to installments collection
+      await FirebaseFirestore.instance.collection('installments').add({
+        'amount': amount,
+        'startDate': Timestamp.fromDate(date),
+        'dueDate': Timestamp.fromDate(date.add(const Duration(days: 30))),
+        'isPaid': false,
+      });
+    } else {
+      // Otherwise, add to expenses collection
+      await FirebaseFirestore.instance.collection('expenses').add({
+        'amount': amount,
+        'category': category,
+        'date': Timestamp.fromDate(date),
+      });
+    }
   }
 
   // Method untuk memuat kategori dan menyimpan ke dalam list _categories
@@ -34,3 +41,4 @@ class CustomCategories {
   // Getter untuk mengambil kategori yang sudah dimuat
   List<String> get categories => _categories;
 }
+
